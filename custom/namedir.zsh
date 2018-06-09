1bd3ef406aa97610a610a9464f7087df7775a611
Michele Sciabarra <sciabarracom@sciabarra.com>
2018-06-09 19:09:40 +0000

    dirs

--- custom/namedir.zsh
+++ custom/namedir.zsh
@@ -1,2 +1,4 @@
 TLM=/Users/msciab/Work/Telmore/2018
 GOTUT=/Users/msciab/Work/OpenWhisk/openwhisk-runtime-go-tutorial
+AP=/home/msciab/go/src/github.com/apache
+SC=/home/msciab/go/src/github.com/sciabarracom
