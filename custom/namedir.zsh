3171626e6bc7d550e3c9a8d0807cd33fa069dd1c
Michele Sciabarra <michele.sciabarra@leonteq.com>
2018-06-15 08:15:56 +0200

    /home/sciam/.oh-my-zsh/custom/misc.zsh

--- custom/namedir.zsh
+++ custom/namedir.zsh
@@ -3,3 +3,4 @@ GOTUT=/Users/msciab/Work/OpenWhisk/openwhisk-runtime-go-tutorial
 AP=/home/msciab/go/src/github.com/apache
 SC=/home/msciab/go/src/github.com/sciabarracom
 IORG=/home/msciab/go/src/github.com/apache/incubator-openwhisk-runtime-go
+PA=/home/sciam/Work/parthenon
