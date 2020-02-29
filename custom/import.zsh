8ade83f22ab9ef7a49800a331d5ad96bc5ac8f6f
Michele Sciabarra <msciabarra@apache.org>
2020-02-29 13:25:18 +0100

    saved

--- custom/import.zsh
+++ custom/import.zsh
@@ -20,14 +20,9 @@ if test -d ~/Library/Python/2.7/bin
 then path+=~/Library/Python/2.7/bin
 fi
 
-if test -e /usr/local/Cellar/go/1.12.7
-then export GOROOT=/usr/local/Cellar/go/1.12.7/libexec
-elif test -e /usr/local/Cellar/go/1.12.5
-then export GOROOT=/usr/local/Cellar/go/1.12.5/libexec
-elif test -e /usr/lib/go-1.10
-then export GOROOT=/usr/lib/go-1.10
-elif test -e /usr/lib/go-1.11
-then export GOROOT=/usr/lib/go-1.11
+GODIRS=(/usr/local/Cellar/go/*/libexec /usr/lib/go-*)
+if test -n "${GODIRS[1]}"
+then export GOROOT="${GODIRS[1]}"
 else echo "No Valid GOROOT"
 fi
 path=($GOROOT/bin $path)
